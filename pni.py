5e65d19e7996adf4dfc9a5ed848b4747a14f2fb9
Michael Koval <koval.michael@gmail.com>
2012-01-23 00:47:52 -0500

    Added BSD license.

--- pni.py
+++ pni.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python
+"""
+Copyright (c) 2012, Michael Koval
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
 
 import collections, crcmod, serial, struct, sys
 from collections import namedtuple
@@ -456,27 +480,5 @@ class FieldforceTCM:
                 continue
             else:
                 raise IOError('Unexpected frame id: {0}.'.format(frame_id))
-
-def main():
-    compass = FieldforceTCM('/dev/ttyUSB0')
-    print 'ModelInfo:',   compass.getModelInfo()
-    print 'Data:',        compass.getData()
-    print 'Params:',      compass.getAcquisitionParams()
-
-    compass.setFilter(32)
-    print 'Filter:',      compass.getFilter()
-
-    compass.setDataComponents([ Component.kHeading ])
-    compass.setAcquisitionParams(True, False, 0.0, 0.1)
-    compass.startStreaming(10.0)
-
-    for t in xrange(0, 10):
-        print compass.getData()
-
-    compass.stopStreaming()
-    compass.close()
-
-if __name__ == '__main__':
-    sys.exit(main())
 
 # vim: set et sw=4 ts=4:
